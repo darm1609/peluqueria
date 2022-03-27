8794efde9d6dec97d2876a9738f3d8b1659bb615
Daniel Alfredo Rodriguez Mendoza <daniel.mendoza@mvm.com.co>
2022-03-27 17:19:37 -0500

    Cambios 2703/2022 5

--- cuadre_de_caja_contenido_lista.php
+++ cuadre_de_caja_contenido_lista.php
@@ -421,6 +421,19 @@
                 $total_ingreso_empleado = 0;
                 $total_ingreso_peluqueria = 0;
                 $total_ingreso_dueño = 0;
+                ?>
+                <form class="w3-container w3-card-4 w3-light-grey w3-margin" method="post">
+                <div class="w3-row  w3-section" style='font-weight: bolder;'>Pagos a empleados</div>
+                <div class="w3-row w3-section">
+                <table border='1' cellpadding='5' cellspacing='0' style='border-color: floralwhite;'>
+                <thead>
+                <tr>
+                <th align="center">Empleado</th>
+                <th align="center">Total</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php
                 foreach ($rows_empleado as $row_empleado)
                 {
                     $sql = "select 
@@ -503,8 +516,6 @@
                                         $porcentaje_empleado = 0;
                                         $porcentaje_dueño = 0;
                                         $porcentaje_peluqueria = 0;
-                                        $acumulado_a_pagar = 0;
-                                        $acumulado_pagado = 0;
                                         
                                         $rows_porcentajes = $result_porcentajes->fetch_all(MYSQLI_ASSOC);
                                         $result_porcentajes->free();
@@ -528,7 +539,7 @@
                                             $total_ingreso_peluqueria += $total_ingreso_linea_peluqueria_porcentaje;
                                             $total_ingreso_dueño += $total_ingreso_linea_dueño_porcentaje;
 
-                                            echo "Fecha: ".$row_ingreso["fecha"]." Ingreso linea: ".$total_ingreso_linea." % empleado: ".$porcentaje_empleado." total empleado: ".$total_ingreso_linea_empleado_porcentaje." % peluqueria: ".$porcentaje_peluqueria ." total peluqueria: ".$total_ingreso_linea_peluqueria_porcentaje." % dueño: ".$porcentaje_dueño." total dueño: ".$total_ingreso_linea_dueño_porcentaje."<br>";
+                                            //echo "Fecha: ".$row_ingreso["fecha"]." Ingreso linea: ".$total_ingreso_linea." % empleado: ".$porcentaje_empleado." total empleado: ".$total_ingreso_linea_empleado_porcentaje." % peluqueria: ".$porcentaje_peluqueria ." total peluqueria: ".$total_ingreso_linea_peluqueria_porcentaje." % dueño: ".$porcentaje_dueño." total dueño: ".$total_ingreso_linea_dueño_porcentaje."<br>";
                                         }
                                     }
                                 }
@@ -540,146 +551,76 @@
                     else
                         unset($result_ingreso);
 
-                    echo $row_empleado["nombre"]." Total ingreso empleado: ".$total_ingreso_empleado." Total ingreso peluqueria: ".$total_ingreso_peluqueria." Total ingreso dueño: ".$total_ingreso_dueño."<br>";
+                    //echo $row_empleado["nombre"]." Total ingreso empleado: ".$total_ingreso_empleado." Total ingreso peluqueria: ".$total_ingreso_peluqueria." Total ingreso dueño: ".$total_ingreso_dueño."<br>";
+
+                    $sql = "select
+                        vp.fecha,
+                        vp.vale_pago,
+                        vp.efectivo,
+                        vp.transferencia,
+                        case
+                            when vp.efectivo = 1 then vpe.monto
+                            else 0
+                        end efectivo_monto,
+                        case
+                            when vp.transferencia = 1 then vpt.monto
+                            else 0
+                        end transferencia_monto
+                    from
+                        vale_pago vp
+                        left join vale_pago_efectivo vpe on vp.id_vale_pago = vpe.id_vale_pago
+                        left join vale_pago_transferencia vpt on vp.id_vale_pago = vpt.id_vale_pago
+                    where
+                        vp.empleado_cedula = '".$row_empleado["empleado_cedula"]."' and vp.fecha_num <= '".$fecha_num_consulta."';";
+                    $result_vale_pago = $bd->mysql->query($sql);
+                    unset($sql);
+                    $total_pagado_a_empleado = 0;
+                    if ($result_vale_pago)
+                    {
+                        if (!empty($result_vale_pago->num_rows))
+                        {
+                            $rows_vale_pago = $result_vale_pago->fetch_all(MYSQLI_ASSOC);
+                            $result_vale_pago->free();
+                            foreach ($rows_vale_pago as $row_vale_pago)
+                            {
+                                $total_pagado_a_empleado += $row_vale_pago["efectivo_monto"];
+                                $total_pagado_a_empleado += $row_vale_pago["transferencia_monto"];
+                            }
+                            unset($rows_vale_pago);
+                        }
+                    }
+                    else
+                        unset($result_vale_pago);
+                    
+                    $total_ingreso_empleado -= $total_pagado_a_empleado;
+                    //echo "<br><br>".$row_empleado["nombre"]." Total ingreso empleado: ".$total_ingreso_empleado." Total ingreso peluqueria: ".$total_ingreso_peluqueria." Total ingreso dueño: ".$total_ingreso_dueño."<br>";
+                    echo "<tr>";
+                    echo "<td><span style='text-decoration: underline; cursor:pointer;' onclick=\"document.getElementById('modal_detalle_empleado_".$row_empleado["empleado_cedula"]."').style.display='block'\">".$row_empleado["nombre"]."</span></td>";
+                    echo "<td align='right'>".$total_ingreso_empleado."</td>";
+                    echo "</tr>";
                 }
+                ?>
+                </tbody>
+                </table>
+                </div>
+                </form>
 
-                // foreach ($rows_empleado as $row_empleado)
-                // {
-                //     $sql = "select
-                //         pg.fecha,
-                //         pg.porcentaje_empleado,
-                //         pg.porcentaje_peluqueria,
-                //         pg.porcentaje_dueño
-                //     from 
-                //         porcentaje_ganancia pg 
-                //     where 
-                //         pg.empleado_cedula = '".$row_empleado["empleado_cedula"]."'
-                //     order by pg.fecha_num desc;";
-                //     $result_porcentajes = $bd->mysql->query($sql);
-                //     unset($sql);
-                //     if ($result_porcentajes)
-                //     {
-                //         if (!empty($result_porcentajes->num_rows))
-                //         {
-                //             $porcentaje_empleado = 0;
-                //             $porcentaje_dueño = 0;
-                //             $porcentaje_peluqueria = 0;
-                //             $acumulado_a_pagar = 0;
-                //             $acumulado_pagado = 0;
-                            
-                //             $rows_porcentajes = $result_porcentajes->fetch_all(MYSQLI_ASSOC);
-                //             $result_porcentajes->free();
-                            
-                //             $porcentaje_empleado = $rows_porcentajes[0]["porcentaje_empleado"];
-                //             $porcentaje_peluqueria = $rows_porcentajes[0]["porcentaje_peluqueria"];
-                //             $porcentaje_dueño = $rows_porcentajes[0]["porcentaje_dueño"];
-
-                //             if (!empty($porcentaje_empleado))
-                //             {
-                //                 $sql = "select 
-                //                 i.fecha_num, 
-                //                 i.id_ingreso, 
-                //                 i.fecha, 
-                //                 mi.motivo, 
-                //                 i.efectivo, 
-                //                 i.transferencia, 
-                //                 i.debito, 
-                //                 i.deuda, 
-                //                 i.observacion, 
-                //                 case 
-                //                     when i.efectivo = 1 then ie.monto 
-                //                     else 0 
-                //                 end efectivo_monto, 
-                //                 case 
-                //                     when i.transferencia = 1 then it.monto 
-                //                     else 0 
-                //                 end transferencia_monto, 
-                //                 case 
-                //                     when i.transferencia = 1 then it.referencia 
-                //                     else '' 
-                //                 end transferencia_referencia, 
-                //                 case 
-                //                     when i.debito = 1 then id.monto 
-                //                     else 0 
-                //                 end debito_monto,
-                //                 e.empleado_cedula, 
-                //                 concat(e.nombre,' ',e.apellido) empleado, 
-                //                 concat(c.nombre,' ',c.apellido) cliente, 
-                //                 case 
-                //                     when i.id_ingreso_padre is not null then 1 
-                //                     else 0 
-                //                 end por_pago_de_deuda 
-                //                 from 
-                //                     ingreso i 
-                //                     inner join motivo_ingreso mi on i.id_motivo_ingreso = mi.id_motivo_ingreso 
-                //                     inner join empleado e on i.empleado_cedula = e.empleado_cedula 
-                //                     left join cliente c on i.cliente_telf = c.telf 
-                //                     left join ingreso_efectivo ie on i.id_ingreso = ie.id_ingreso 
-                //                     left join ingreso_transferencia it on i.id_ingreso = it.id_ingreso 
-                //                     left join ingreso_debito id on id.id_ingreso = i.id_ingreso 
-                //                 where 
-                //                     i.empleado_cedula = '".$row_empleado["empleado_cedula"]."' and (i.efectivo != 0 or i.debito != 0 or i.transferencia != 0 or i.deuda != 1);";
-                //                 $result_ingreso = $bd->mysql->query($sql);
-                //                 unset($sql);
-                //                 if ($result_ingreso)
-                //                 {
-                //                     if (!empty($result_ingreso->num_rows))
-                //                     {
-                //                         $rows_ingreso = $result_ingreso->fetch_all(MYSQLI_ASSOC);
-                //                         $result_ingreso->free();
-                //                         $total_ingreso = 0;
-                //                         $total_ingreso_linea = 0;
-                //                         $total_ingreso_linea_empleado_porcentaje = 0;
-                //                         $total_ingreso_linea_peluqueria_porcentaje = 0;
-                //                         $total_ingreso_linea_dueño_porcentaje = 0;
-                //                         foreach ($rows_ingreso as $row_ingreso)
-                //                         {
-                //                             $total_ingreso_linea = 0;
-                //                             $total_ingreso_linea += $row_ingreso["efectivo_monto"];
-                //                             $total_ingreso_linea += $row_ingreso["transferencia_monto"];
-                //                             $total_ingreso_linea += $row_ingreso["debito_monto"];
-                //                             $total_ingreso += $total_ingreso_linea;
-                //                             $total_ingreso_linea_empleado_porcentaje += ($porcentaje_empleado * $total_ingreso_linea) / 100;
-                //                             $total_ingreso_linea_peluqueria_porcentaje += ($porcentaje_peluqueria * $total_ingreso_linea) / 100;
-                //                             $total_ingreso_linea_dueño_porcentaje += ($porcentaje_dueño * $total_ingreso_linea) / 100;
-                //                         }
-                //                         $sql = "select
-                //                             vp.vale_pago,
-                //                             vp.fecha,
-                //                             vp.efectivo,
-                //                             vp.transferencia,
-                //                             case 
-                //                                 when vp.efectivo = 1 then vpe.monto 
-                //                                 else 0 
-                //                             end efectivo_monto, 
-                //                             case 
-                //                                 when vp.transferencia = 1 then vpt.monto 
-                //                                 else 0 
-                //                             end transferencia_monto
-                //                         from
-                //                             vale_pago vp
-                //                             left join vale_pago_efectivo vpe on vp.id_vale_pago = vpe.id_vale_pago
-                //                             left join vale_pago_transferencia vpt on vp.id_vale_pago = vpt.id_vale_pago
-                //                         where
-                //                             vp.empleado_cedula = '".$row_empleado["empleado_cedula"]."';";
-
-                //                         echo $sql;
-                //                         // echo "Total del empleado: ".$total_ingreso."<br>";
-                //                         // echo "Total % del empleado: ".$porcentaje_empleado." Total con porcentaje: ".$total_ingreso_linea_empleado_porcentaje."<br>";
-                //                         // echo $total_ingreso_linea_peluqueria_porcentaje."<br>";
-                //                         // echo $total_ingreso_linea_dueño_porcentaje."<br>";
-                //                         unset($rows_ingreso, $total_ingreso, $total_ingreso_linea, $total_ingreso_linea_empleado_porcentaje, $total_ingreso_linea_peluqueria_porcentaje, $total_ingreso_linea_dueño_porcentaje);
-                //                     }
-                //                 }
-                //                 else
-                //                     unset($sql);
-                //             }
-                //             unset($porcentaje_empleado,$porcentaje_dueño,$porcentaje_peluqueria);
-                //         }
-                //     }
-                //     else
-                //         unset($result_porcentajes);
-                // }
+                <?php
+                //Modal de detalles pagos a empleados
+                foreach ($rows_empleado as $row_empleado)
+                {
+                    echo "<div id='modal_detalle_empleado_".$row_empleado["empleado_cedula"]."' class='w3-modal'>";
+                    echo "<div class='w3-modal-content'>";
+                    echo "<span onclick=\"document.getElementById('modal_detalle_empleado_".$row_empleado["empleado_cedula"]."').style.display='none'\" class='w3-button w3-display-topright'>&times;</span>";
+                    echo "<br>";
+                    echo "<div style='padding: 1em;'>";
+                    echo "<p>".$row_empleado["nombre"]."</p>";
+                    echo "</div>";
+                    echo "</div>";
+                    echo "</div>";
+                }
+                //Fin de modal de detalles pagos a empleados
+                
                 unset($rows_empleado);
             }
         }
