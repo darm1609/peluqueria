ba1443f54e0097e86b272de6e7f2a45938837525
Daniel Alfredo Rodriguez Mendoza <daniel.mendoza@mvm.com.co>
2022-04-18 13:50:50 -0500

    Cambios 18-04-2022 1

--- cuadre_de_caja_contenido_lista.php
+++ cuadre_de_caja_contenido_lista.php
@@ -15,6 +15,12 @@
     }
 
 </script>
+<style>
+    .table-overflow {
+        overflow-x: scroll;
+        overflow-x: auto;
+    }
+</style>
 <?php
 	session_start();
 	require("head.php");
@@ -1563,33 +1569,343 @@
         <?php
     }
 
-    function mostrar_busqueda($bd)
+    function consultar_ingresos_totales($bd, &$array_ingresos)
+    {
+        //Ingresos acumulados
+        $sql = "select 
+        i.fecha_num, 
+        i.id_ingreso, 
+        i.fecha, 
+        mi.motivo, 
+        i.efectivo, 
+        i.transferencia, 
+        i.debito, 
+        i.deuda, 
+        i.observacion, 
+        case 
+            when i.efectivo = 1 then ie.monto 
+            else '' 
+        end efectivo_monto, 
+        case 
+            when i.transferencia = 1 then it.monto 
+            else '' 
+        end transferencia_monto, 
+        case 
+            when i.transferencia = 1 then it.referencia 
+            else '' 
+        end transferencia_referencia, 
+        case 
+            when i.debito = 1 then id.monto 
+            else '' 
+        end debito_monto, 
+        concat(e.nombre,' ',e.apellido) empleado, 
+        concat(c.nombre,' ',c.apellido) cliente, 
+        case 
+            when i.id_ingreso_padre is not null then 1 
+            else 0 
+        end por_pago_de_deuda 
+        from 
+            ingreso i 
+            inner join motivo_ingreso mi on i.id_motivo_ingreso = mi.id_motivo_ingreso 
+            inner join empleado e on i.empleado_telf = e.empleado_telf 
+            left join cliente c on i.cliente_telf = c.telf 
+            left join ingreso_efectivo ie on i.id_ingreso = ie.id_ingreso 
+            left join ingreso_transferencia it on i.id_ingreso = it.id_ingreso 
+            left join ingreso_debito id on id.id_ingreso = i.id_ingreso 
+        where 
+            (i.efectivo != 0 or i.debito != 0 or i.transferencia != 0 or i.deuda != 1) 
+        union all 
+        select 
+            v.fecha_num, 
+            v.id_venta as id_ingreso, 
+            v.fecha, 
+            v.motivo, 
+            v.efectivo, 
+            v.transferencia, 
+            v.debito, 
+            v.deuda, 
+            '' as observacion, 
+            case 
+                when v.efectivo = 1 then ve.monto 
+                else '' 
+            end efectivo_monto, 
+            case 
+                when v.transferencia = 1 then vt.monto 
+                else '' 
+            end transferencia_monto, 
+            case 
+                when v.transferencia = 1 then vt.referencia 
+                else '' 
+            end transferencia_referencia, 
+            case 
+                when v.debito = 1 then vd.monto 
+                else '' 
+            end debito_monto, 
+            'Venta' empleado, 
+            concat(c.nombre,' ',c.apellido) cliente, 
+            case 
+                when v.id_venta_padre is not null then 1 
+                else 0 
+            end por_pago_de_deuda 
+        from 
+            venta v 
+            left join cliente c on v.cliente_telf = c.telf 
+            left join venta_efectivo ve on v.id_venta = ve.id_venta 
+            left join venta_transferencia vt on v.id_venta = vt.id_venta 
+            left join venta_debito vd on vd.id_venta = v.id_venta 
+        where 
+            (v.efectivo != 0 or v.debito != 0 or v.transferencia != 0 or v.deuda != 1)
+        union all
+        select
+            ap.fecha_num,
+            ap.id_abono_peluqueria as id_ingreso, 
+            ap.fecha,
+            'Abono a pelqueria' as motivo,
+            ap.efectivo,
+            ap.transferencia,
+            '' debito,
+            '' deuda,
+            '' as observacion,
+            case
+                when ap.efectivo = 1 then ape.monto
+                else 0
+            end efectivo_monto,
+            case
+                when ap.transferencia = 1 then apt.monto
+                else 0
+            end transferencia_monto,
+            case 
+                when ap.transferencia = 1 then apt.referencia 
+                else '' 
+            end transferencia_referencia, 
+            0 debito_monto,
+            '' empleado,
+            '' cliente,
+            '' por_pago_de_deuda
+        from
+            abono_peluqueria ap
+            left join abono_peluqueria_efectivo ape on ap.id_abono_peluqueria = ape.id_abono_peluqueria
+            left join abono_peluqueria_transferencia apt on ap.id_abono_peluqueria = apt.id_abono_peluqueria
+        where 
+            (ap.efectivo != 0 or ap.transferencia != 0)
+        order by fecha_num asc;";
+        $result = $bd->mysql->query($sql);
+        unset($sql);
+        if ($result)
+        {
+            if (!empty($result->num_rows))
+            {
+                $array_ingresos = $result->fetch_all(MYSQLI_ASSOC);
+                $result->free();
+            }
+        }
+        else
+            unset($result);
+    }
+
+    function consultar_egresos_totales($bd, &$array_egresos)
+    {
+        $sql = "select
+            e.fecha_num,
+            e.id_egreso id,
+            e.fecha,
+            e.motivo,
+            e.efectivo, 
+            e.transferencia, 
+            e.debito, 
+            case 
+                when e.efectivo = 1 then ee.monto 
+                else 0 
+            end efectivo_monto, 
+            case 
+                when e.transferencia = 1 then et.monto 
+                else 0 
+            end transferencia_monto, 
+            case 
+                when e.transferencia = 1 then et.referencia 
+                else '' 
+            end transferencia_referencia, 
+            case 
+                when e.debito = 1 then ed.monto 
+                else 0 
+            end debito_monto, 
+            '' empleado
+        from
+            egreso e
+            left join egreso_debito ed on e.id_egreso = ed.id_egreso
+            left join egreso_efectivo ee on e.id_egreso = ee.id_egreso
+            left join egreso_transferencia et on e.id_egreso = et.id_egreso
+        union all
+        select
+            vp.fecha_num,
+            vp.id_vale_pago id,
+            vp.fecha,
+            vp.vale_pago motivo,
+            vp.efectivo,
+            vp.transferencia,
+            0 debito,
+            case 
+                when vp.efectivo = 1 then vpe.monto 
+                else 0 
+            end efectivo_monto, 
+            case 
+                when vp.transferencia = 1 then vpt.monto 
+                else 0 
+            end transferencia_monto, 
+            case 
+                when vp.transferencia = 1 then vpt.referencia 
+                else '' 
+            end transferencia_referencia, 
+            0 debito_monto, 
+            concat(e.nombre,' ',e.apellido) empleado
+        from
+            vale_pago vp
+            inner join empleado e on vp.empleado_telf = e.empleado_telf
+            left join vale_pago_efectivo vpe on vp.id_vale_pago = vpe.id_vale_pago
+            left join vale_pago_transferencia vpt on vp.id_vale_pago = vpt.id_vale_pago
+        union all
+        select 
+            ae.fecha_num,
+            ae.id_abono_empleado id,
+            ae.fecha,
+            'Abono a empleado' motivo,
+            ae.efectivo,
+            ae.transferencia,
+            0 debito,
+            case 
+                when ae.efectivo = 1 then aee.monto 
+                else 0 
+            end efectivo_monto, 
+            case 
+                when ae.transferencia = 1 then aet.monto 
+                else 0 
+            end transferencia_monto, 
+            case 
+                when ae.transferencia = 1 then aet.referencia 
+                else '' 
+            end transferencia_referencia, 
+            0 debito_monto, 
+            concat(e.nombre,' ',e.apellido) empleado
+        from
+            abono_empleado ae
+            inner join empleado e on ae.empleado_telf = e.empleado_telf
+            left join abono_empleado_efectivo aee on ae.id_abono_empleado = aee.id_abono_empleado
+            left join abono_empleado_transferencia aet on ae.id_abono_empleado = aet.id_abono_empleado
+        order by fecha_num asc;";
+        $result = $bd->mysql->query($sql);
+        unset($sql);
+        if ($result)
+        {
+            if (!empty($result->num_rows))
+            {
+                $array_egresos = $result->fetch_all(MYSQLI_ASSOC);
+                $result->free();
+            }
+        }
+        else
+            unset($result);
+    }
+
+    function consultar_porcentaje_empleados_totales($bd, &$array_porcentajes)
+    {
+        $sql = "select
+            e.empleado_telf,
+            concat(e.nombre,' ',e.apellido) nombre_empleado,
+            pg.id_porcentaje_ganancia,
+            pg.fecha_num,
+            pg.fecha,
+            pg.porcentaje_empleado,
+            pg.porcentaje_peluqueria,
+            pg.porcentaje_dueño
+        from 
+            porcentaje_ganancia pg
+            inner join empleado e on pg.empleado_telf = e.empleado_telf
+        order by pg.fecha_num;";
+        $result = $bd->mysql->query($sql);
+        unset($sql);
+        if ($result)
+        {
+            if (!empty($result->num_rows))
+            {
+                $array_porcentajes = $result->fetch_all(MYSQLI_ASSOC);
+                $result->free();
+            }
+        }
+        else
+            unset($result);
+    }
+
+    function mostrar_ingresos_netos_del_dia($array_ingresos)
+    {
+        ?>
+        <form class="w3-container w3-card-4 w3-light-grey w3-margin table-overflow" method="post">
+            <div class="w3-row  w3-section" style='font-weight: bolder;'>Ingresos netos del d&iacute;a</div>
+            <div class="w3-row w3-section">
+                <table class="w3-table w3-striped w3-bordered w3-border w3-hoverable w3-white">
+                    <thead>
+                        <tr class="w3-dulcevanidad">
+                            <th align="center">Tipo</th>
+                            <th align="center">Empleado</th>
+                            <th align="center">Efectivo</th>
+                            <th align="center">Dat&aacute;fono</th>
+                            <th align="center">Transferencia</th>
+                            <th align="center">Referencia</th>
+                            <th align="center">Total</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <?php
+    }
+
+    function mostrar_busqueda($bd, &$array_ingresos, &$array_egresos, &$array_porcentajes)
     {
         $admin = usuario_admin();
         $cajero = usuario_cajero();
         $consulta = usuario_consulta();
 
-        if ($admin) {
-            acumulado_peluqueria_dueño($bd);
-        }
+        consultar_ingresos_totales($bd, $array_ingresos);
 
-        resumen_del_dia($bd);
+        consultar_egresos_totales($bd, $array_egresos);
 
-        ingresos_del_dia($bd);
+        consultar_porcentaje_empleados_totales($bd, $array_porcentajes);
 
-        ventas_de_productos_del_dia($bd);
+        $fecha_num_consulta = strtotime($_POST["bfecha"][6].$_POST["bfecha"][7].$_POST["bfecha"][8].$_POST["bfecha"][9]."-".$_POST["bfecha"][3].$_POST["bfecha"][4]."-".$_POST["bfecha"][0].$_POST["bfecha"][1]);
+        $fecha = $_POST["bfecha"];
 
-        deudas_del_dia($bd);
+        mostrar_ingresos_netos_del_dia($array_ingresos);
+        
 
-        egresos_pagos_vales($bd);
+        // if ($admin) {
+        //     acumulado_peluqueria_dueño($bd);
+        // }
 
-        acumulado_por_empleado($bd);
+        // resumen_del_dia($bd);
+
+        // ingresos_del_dia($bd);
+
+        // ventas_de_productos_del_dia($bd);
+
+        // deudas_del_dia($bd);
+
+        // egresos_pagos_vales($bd);
+
+        // acumulado_por_empleado($bd);
     }
 
     global $servidor, $puerto, $usuario, $pass, $basedatos;
 	$bd=new BaseDatos($servidor,$puerto,$usuario,$pass,$basedatos);
 	if($bd->conectado)
 	{
-        mostrar_busqueda($bd);
+        $array_ingresos = array();
+        $array_egresos = array();
+        $array_porcentajes = array();
+        mostrar_busqueda($bd, $array_ingresos, $array_egresos, $array_porcentajes);
     }
 ?>
