3c013ad69e99c22ab590143dd2e308093c71ea25
Daniel Alfredo Rodriguez Mendoza <daniel.mendoza@mvm.com.co>
2022-04-25 07:57:40 -0500

    Cambios 25/04/2022 1

---
+++ config_public.php
@@ -0,0 +1,15 @@
+<?php	
+	global $servidor;
+	global $puerto;
+	global $usuario;
+	global $pass;
+	global $basedatos;
+	$servidor='localhost';
+	$puerto='3306';
+	$usuario='vinkasof_dulcevanidad_user';
+	$pass='DTU#wqnm?+]g';
+	$basedatos='vinkasof_dulcevanidad';
+	ini_set("display_errors","Off");
+	date_default_timezone_set("America/Bogota");
+	setlocale(LC_MONETARY, 'es_CO');
+?>
